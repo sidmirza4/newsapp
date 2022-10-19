57bcbb62bd88ee10111bf75784de13f6af2c2331
Mohd Shahid <sidmirza4@gmail.com>
2022-10-19 07:25:28 +0530

    add proxy url

--- axios.js
+++ axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const base = "https://newsapi.org/v2/";
+
 const axiosInstance = axios.create({
-	baseURL: "https://newsapi.org/v2",
+	baseURL: `${proxy}${base}`,
 	headers: {
 		Authorization: import.meta.env.VITE_NEWS_API_KEY,
 	},
