2704a8cc5f60984a4bbe584283b61b8f4c1eb771
Mohd Shahid <sidmirza4@gmail.com>
2022-10-19 07:31:54 +0530

    remove proxy

--- axios.js
+++ axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const proxy = "https://cors-anywhere.herokuapp.com/";
 const base = "https://newsapi.org/v2/";
 
 const axiosInstance = axios.create({
-	baseURL: `${proxy}${base}`,
+	baseURL: `${base}`,
 	headers: {
 		Authorization: import.meta.env.VITE_NEWS_API_KEY,
 	},
